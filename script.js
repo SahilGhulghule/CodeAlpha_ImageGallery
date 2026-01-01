64ccb3230a97ba65ceedd07d5bb7fca1df4a8368
SahilGhulghule <sahilghulghule401@gmail.com>
2026-01-01 18:53:39 +0530

    second commit

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const images = [
-    // Nature 0-9
+   
     "https://picsum.photos/id/10/1200/900", "https://picsum.photos/id/11/1200/900", "https://picsum.photos/id/12/1200/900", "https://picsum.photos/id/13/1200/900", "https://picsum.photos/id/14/1200/900",
     "https://picsum.photos/id/15/1200/900", "https://picsum.photos/id/16/1200/900", "https://picsum.photos/id/17/1200/900", "https://picsum.photos/id/18/1200/900", "https://picsum.photos/id/19/1200/900",
-    // City 10-19
+    
     "https://picsum.photos/id/103/1200/900", "https://picsum.photos/id/122/1200/900", "https://picsum.photos/id/125/1200/900", "https://picsum.photos/id/133/1200/900", "https://picsum.photos/id/142/1200/900",
     "https://picsum.photos/id/145/1200/900", "https://picsum.photos/id/158/1200/900", "https://picsum.photos/id/164/1200/900", "https://picsum.photos/id/180/1200/900", "https://picsum.photos/id/192/1200/900"
 ];
